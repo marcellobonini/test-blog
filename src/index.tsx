bcd4a15ba0d2ae60636637cc7441129de0eb1f3d
marcellobonini <marcello.bonini@protonmail.com>
2023-07-18 23:57:18 +0200

    added ui library

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from "./components/theme-provider"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+      <App/>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
